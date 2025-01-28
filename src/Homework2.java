c62a7dae0de767d5901164e72100290e09895741
VideoNLE7 <vasilyevdanila00@mail.ru>
2025-01-28 10:04:28 +0300

    my homework 2

--- src/Homework2.java
+++ src/Homework2.java
@@ -32,21 +32,12 @@ public class Homework2 {
 
         System.out.println("Отсортированный список:");
 
+        //Создаем компоратор для сравнения примитивных типов
         Comparator<Object> comparator = (o1, o2) -> {
-            // Сравнение строк
-            if (o1 instanceof String && o2 instanceof String) {
-                return ((String) o1).compareTo((String) o2);
+            if (o1 instanceof Comparable && o2 instanceof Comparable) {
+                return ((Comparable<Object>) o1).compareTo(o2);
             }
-            // Сравнение чисел
-            if (o1 instanceof Integer && o2 instanceof Integer) {
-                return ((Integer) o1).compareTo((Integer) o2);
-            }
-            // Сравнение символов
-            if (o1 instanceof Character && o2 instanceof Character) {
-                return ((Character) o1).compareTo((Character) o2);
-            }
-            // Если типы разные, приводим их к строке и сравниваем
-            return String.valueOf(o1).compareTo(String.valueOf(o2));
+            throw new IllegalArgumentException("Objects are not comparable");
         };
 
         list.sort(comparator);
@@ -56,6 +47,7 @@ public class Homework2 {
         list.clear();
         System.out.println("Список после очистки методом remove");
         list.getList(list);
+
         // Добавляем элементы в список для сортировки методом quicksort
         list.add(3);
         list.add(1);
@@ -69,6 +61,19 @@ public class Homework2 {
         System.out.println("Список отсортированный методом quicksort:");
         quickSort(list,0, list.size-1,comparator);
         list.getList(list);
+        list.clear();
+
+        System.out.println("Неотсортированный список типа String");
+        list.add("Banana");
+        list.add("Apple");
+        list.add("Strowberry");
+        list.add("Kiwi");
+        list.add("Orange");
+        list.getList(list);
+
+        System.out.println("Отсортированный список String");
+        quickSort(list,0, list.size()-1,comparator);
+        list.getList(list);
     }
 
 
@@ -79,16 +84,19 @@ public class Homework2 {
         private Object[] elements;
         private int size;
 
+        //Создаем конструктор класса MyArrayList
         public MyArrayList(){
             elements = new Object[5];
             size = 0;
         }
 
+        //Метод add позваоляет добавлять элемент в конец списка
         public void add(T element){
             ensureCapacity();
             elements[size++] = element;
         }
 
+        //Метод add позваоляет добавлять элемент по индексу
         public void add(int index, T element) {
             if (index < 0 ) {
                 throw new IndexOutOfBoundsException("Invalid index");
@@ -103,27 +111,32 @@ public class Homework2 {
             elements[index] = element;
         }
 
+        //Метод get позваоляет получить элемент по индексу
         public T get (int index){
             if(index < 0 || index >= size)
                 throw new IndexOutOfBoundsException("Invalid index");
             return (T) elements[index];
         }
 
-
+        //Метод size возвращает размер списка
         public int size(){
             return size;
         }
 
+        //Внутренний метод ensureCapacity позволяет увеличить список, если мы достигли лимита
         private void ensureCapacity() {
             elements = java.util.Arrays.copyOf(elements,elements.length * 2);
         }
 
+        //Внутренний метод getList выводит переданный список
         private static void getList(MyArrayList list){
             for (int i = 0; i < list.size; i++) {
                 System.out.println(list.get(i) + " ");
             }
         }
 
+        //Метод remove удаляет элемент по индексу, сдвигает элементы после удаленного влева и очищает
+        // ссылку крайнего правого (пустого) элемента
         public void remove(int index){
             if(index < 0 || index>= size)
                 throw new IndexOutOfBoundsException("Invalid index");
@@ -134,6 +147,7 @@ public class Homework2 {
             size--;
         }
 
+        //Метод clear очищает ссылки элементов и уменьшает размер до нуля
         public void clear(){
             for (int i = 0; i < size; i++) {
                 elements[i] = null;
@@ -141,6 +155,7 @@ public class Homework2 {
             size = 0;
         }
 
+        //Метод sort сортирует элементы в массиве elements с использованием переданного компаратора
         public void sort(Comparator<T> comporator){
             if (size <= 1){
                 return;
